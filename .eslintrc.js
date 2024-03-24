87427cd4b02cd958aa20a0fb010a134e26c55321
seongsoo96 <seongsoo.dev@gmail.com>
2024-03-24 18:07:25 +0900

    :heavy_plus_sign: Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended",
-        "prettier"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        'react/react-in-jsx-scope': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/naming-convention': 'off',
-        '@typescript-eslint/no-floating-promises': 'off',
-        '@typescript-eslint/strict-boolean-expressions': 'off',
-    },
-    ignorePatterns: ['node_modules/', '*.js', '*.d.ts'],
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+  },
+  ignorePatterns: ['node_modules/', '*.js', '*.d.ts'],
+};
--- src/app/(main)/index.tsx
+++ src/app/(main)/index.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from 'react-native';
 
-import { Text, View } from "@/components/Themed"
+import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
@@ -12,22 +12,22 @@ export default function TabOneScreen() {
         darkColor="rgba(255,255,255,0.1)"
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: "80%",
+    width: '80%',
   },
-})
+});
