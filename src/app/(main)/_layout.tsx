9457fa9016ecac6995154bcdbb370bc080043e03
seongsoo96 <seongsoo.dev@gmail.com>
2024-03-24 17:14:43 +0900

    :art: Set folder structure

--- src/app/(main)/_layout.tsx
+++ src/app/(main)/_layout.tsx
@@ -1,11 +1,9 @@
-import React from "react"
 import FontAwesome from "@expo/vector-icons/FontAwesome"
-import { Link, Tabs } from "expo-router"
-import { Pressable } from "react-native"
+import { Tabs } from "expo-router"
 
 import Colors from "@/src/constants/Colors"
-import { useColorScheme } from "@/src/components/useColorScheme"
-import { useClientOnlyValue } from "@/src/components/useClientOnlyValue"
+import { useClientOnlyValue } from "@/src/hooks/useClientOnlyValue"
+import { useColorScheme } from "@/src/hooks/useColorScheme"
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -32,20 +30,6 @@ export default function TabLayout() {
         options={{
           title: "Tab One",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
         }}
       />
       <Tabs.Screen
