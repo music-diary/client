69f6ef8776221b56e7bcad922f8214bcc9a13fbe
seongsoo96 <seongsoo.dev@gmail.com>
2024-03-24 17:14:43 +0900

    :art: Set folder structure

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,7 +9,7 @@ import { Stack } from "expo-router"
 import * as SplashScreen from "expo-splash-screen"
 import { useEffect } from "react"
 
-import { useColorScheme } from "@/src/components/useColorScheme"
+import { useColorScheme } from "@/src/hooks/useColorScheme"
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -18,7 +18,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+  initialRouteName: "(main)",
 }
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -54,8 +54,7 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        <Stack.Screen name="(main)" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
   )
