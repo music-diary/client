ef1ed2021e2833d3353b94f2434031a2d6318e40
TaegeonKim <61930500+Taegon21@users.noreply.github.com>
2024-03-25 21:15:52 +0900

    🎨 Set up basic structure and font (#3)
    * :package: Add yarn.lock file
    
    Removed package-lock.json and created yarn.lock to transition from npm to yarn for dependency management.
    
    * :art: Add Pretendard font
    
    * :art: Setup basic structure
    
    Constructed the basic architecture of the application with the main sections: Home, Archive, Letter, and MyPage.

---
+++ src/app/(main)/archive/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const ArchiveScreen = () => {
+  return (
+    <View>
+      <Text>아카이브</Text>
+    </View>
+  );
+};
+
+export default ArchiveScreen;
---
+++ src/app/(main)/letter/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const LetterScreen = () => {
+  return (
+    <View>
+      <Text>편지</Text>
+    </View>
+  );
+};
+
+export default LetterScreen;
