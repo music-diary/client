9f7a24bdfaf23c18bf0ca33bc776dc95b56eaad1
TaegeonKim <aksen5240@gmail.com>
2024-04-04 21:07:05 +0900

    :art: Set common font

---
+++ src/constants/Fonts.ts
@@ -0,0 +1,34 @@
+export default {
+  h1: {
+    fontFamily: 'pret-b',
+    fontSize: 20,
+  },
+  t1: {
+    fontFamily: 'pret-b',
+    fontSize: 16,
+  },
+  b1: {
+    fontFamily: 'pret',
+    fontSize: 16,
+  },
+  b1_sb: {
+    fontFamily: 'pret-sb',
+    fontSize: 16,
+  },
+  b2: {
+    fontFamily: 'pret',
+    fontSize: 14,
+  },
+  b2_sb: {
+    fontFamily: 'pret-sb',
+    fontSize: 14,
+  },
+  btn: {
+    fontFamily: 'pret',
+    fontSize: 12,
+  },
+  lb: {
+    fontFamily: 'pret-sb',
+    fontSize: 10,
+  },
+};
