f57b0052aef55a9061279540fb7d5f159e41bdfa
TaegeonKim <61930500+Taegon21@users.noreply.github.com>
2024-04-12 01:51:36 +0900

    :sparkles: Implement home screen for new user (#10)
    * :sparkles: Add home component for TopDescription based on monthly entries
    
    Added a home component to distinguish TopDescription based on the number of monthly entries: more than two and fewer than two.
    
    * :sparkles: Customize home screen based on user status and activity
    
    Configured the home screen dynamically to display based on new user status, monthly diary entries, and received letters count.

--- src/components/home/TopDescription.tsx
+++ src/components/home/TopDescription.tsx
@@ -2,11 +2,17 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Colors from '@/constants/Colors';
 import Fonts from '@/constants/Fonts';
 
-const TopDescription = () => {
+const formatKST = (date: Date) => {
+  const offset = 1000 * 60 * 60 * 9; // UTC+9
+  const koreaDate = new Date(date.getTime() + offset);
+  return koreaDate.toISOString().split('T')[0];
+};
+
+const lessThanTwo = ({ name }: { name: string }) => {
   return (
     <View style={styles.topDescription}>
       <Text style={styles.topMent}>
-        Miya님,{'\n'}오늘 하루를{' '}
+        {name}님,{'\n'}오늘 하루를{' '}
         <Text style={styles.highlightText}>
           노래와{'\n'}함께 기록{''}
         </Text>
@@ -17,6 +23,33 @@ const TopDescription = () => {
       </TouchableOpacity>
     </View>
   );
+};
+const moreThanTwo = ({ count, name }: { count: number; name: string }) => {
+  const date = new Date();
+  const koreaTime = formatKST(date);
+  const month = parseInt(koreaTime.split('-')[1], 10);
+
+  return (
+    <View style={styles.topDescription}>
+      <Text style={styles.topMent}>
+        {name}님은 {month}월 한달{'\n'}
+        <Text style={styles.highlightText}>
+          {count}개의 하루{''}
+        </Text>
+        를{'\n'}노래와 함께 했어요
+      </Text>
+      <TouchableOpacity style={styles.topButton}>
+        <Text style={styles.topButtonText}>일기쓰러 가기</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const TopDescription = ({ count, name }: { count: number; name: string }) => {
+  if (count < 2) {
+    return lessThanTwo({ name });
+  }
+  return moreThanTwo({ count, name });
 };
 
 export default TopDescription;
