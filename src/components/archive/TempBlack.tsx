e307eb60d01b3cd0247f0efe2e309a75ba497e3b
TaegeonKim <61930500+Taegon21@users.noreply.github.com>
2024-05-08 11:41:12 +0900

    ✨ Integrated monthly calendar into archive page (#17)
    * :heavy_plus_sign: Added react-native-calendars library
    
    * :sparkles: Integrated monthly calendar into archive page

---
+++ src/components/archive/TempBlack.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ActivityIndicator, StyleSheet } from 'react-native';
+import Colors from '@/constants/Colors';
+
+const TempBlack = () => {
+  return <ActivityIndicator style={styles.container} color="#793FB5" />;
+};
+
+export default TempBlack;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.black,
+    zIndex: 10,
+  },
+});
