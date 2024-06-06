f1924e82affb613919a2569bb4867dee42e8cf9d
TaegeonKim <61930500+Taegon21@users.noreply.github.com>
2024-06-06 10:46:14 +0900

    ✨ Implement routeswitcher for enhanced navigation on archive page (#20)
    * ✨ Implement routeswitcher component
    
    * :art: Change stack structure of archive page
    
    * ✨ Add routeswitcher component for navigation between views

--- src/app/(main)/archive/month/[month].tsx
+++ src/app/(main)/archive/month/[month].tsx
@@ -6,6 +6,7 @@ import dummy_archive_month from '@/data/dummy_archive_month.json';
 import DailyMainArchive from '@/components/archive/DailyMainArchive';
 import dummy_archive_recommend from '@/data/dummy_archive_recommend.json';
 import RecommendMusic from '@/components/archive/RecommendMusic';
+import RouteSwitcher from '@/components/archive/RouteSwitcher';
 
 interface DiaryData {
   id: string;
@@ -36,30 +37,35 @@ const MonthScreen = () => {
   }, []);
 
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.headerText}>{month}</Text>
-      <ScrollView
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        style={{ paddingTop: 11 }}
-      >
-        <View style={styles.scrollContent}>
-          {entryData.map((entry) => (
-            <DailyMainArchive key={entry.id} {...entry} />
-          ))}
-        </View>
-      </ScrollView>
-      <View style={styles.recommendContainer}>
-        <Text style={styles.headerText}>Miya님이 3월에 추천받은 음악들</Text>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+    <>
+      <View style={styles.header}>
+        <RouteSwitcher />
+      </View>
+      <ScrollView style={styles.container}>
+        <Text style={styles.headerText}>{month}</Text>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          style={{ paddingTop: 11 }}
+        >
           <View style={styles.scrollContent}>
-            {recommendData.map((entry) => (
-              <RecommendMusic key={entry.id} {...entry} />
+            {entryData.map((entry) => (
+              <DailyMainArchive key={entry.id} {...entry} />
             ))}
           </View>
         </ScrollView>
-      </View>
-    </ScrollView>
+        <View style={styles.recommendContainer}>
+          <Text style={styles.headerText}>Miya님이 3월에 추천받은 음악들</Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View style={styles.scrollContent}>
+              {recommendData.map((entry) => (
+                <RecommendMusic key={entry.id} {...entry} />
+              ))}
+            </View>
+          </ScrollView>
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
@@ -72,6 +78,9 @@ const styles = StyleSheet.create({
     paddingLeft: 16,
     paddingTop: 20,
   },
+  header: {
+    height: 50,
+  },
   headerText: {
     color: Colors.white,
     fontFamily: 'pret-b',
@@ -79,10 +88,10 @@ const styles = StyleSheet.create({
   },
   scrollContent: {
     flexDirection: 'row',
-
     gap: 10,
   },
   recommendContainer: {
     paddingTop: 30,
+    paddingBottom: 150,
   },
 });
