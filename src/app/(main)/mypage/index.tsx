2657fb80c9789262b4d8d854d6abdf1feb882bde
TaegeonKim <aksen5240@gmail.com>
2024-06-16 01:19:01 +0900

    💄 Update my page main screen ui with recent changes

--- src/app/(main)/mypage/index.tsx
+++ src/app/(main)/mypage/index.tsx
@@ -7,35 +7,45 @@ import {
   ScrollView,
 } from 'react-native';
 import { useRouter } from 'expo-router';
-import { MaterialIcons } from '@expo/vector-icons';
+import { Feather, MaterialIcons } from '@expo/vector-icons';
 import Colors from '@/constants/Colors';
 import Fonts from '@/constants/Fonts';
-import HeaderContentItem from '@/components/mypage/HeaderContentItem';
 import BodyNavigator from '@/components/mypage/BodyNavigator';
 import CustomToggle from '@/components/common/CustomToggle';
 import CustomAlert from '@/components/common/CustomAlert';
 
 const MypageScreen = () => {
-  // 장르 추천 토글
-  const [isToggled, setIsToggled] = useState(false);
   const router = useRouter();
+  // 날짜
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+
+  /* 토글 설정 */
+  // 장르 추천 토글
+  const [isGenreToggled, setIsGenreToggled] = useState<boolean>(false);
   const handleToggleChange = (state: boolean) => {
-    setIsToggled(state);
+    setIsGenreToggled(state);
   };
 
-  // /* onpress시 routing 모음 /*
-  // onpress시 archive로 이동
-  const onPressArchive = () => {
-    router.push('/(main)/archive');
+  // 일기 알림 토글
+  const [isDiaryToggled, setIsDiaryToggled] = useState<boolean>(true);
+  const handleDiaryToggleChange = (state: boolean) => {
+    setIsDiaryToggled(state);
   };
-  // onpress시 letter로 이동
-  const onPressLetter = () => {
-    router.push('/(main)/letter');
+
+  // 기타 알림 토글
+  const [isEtcToggled, setIsEtcToggled] = useState<boolean>(false);
+  const handleEtcToggleChange = (state: boolean) => {
+    setIsEtcToggled(state);
   };
-  // onpress시 myfriend로 이동
-  const onPressMyfriend = () => {
-    router.push('/(main)/mypage/myfriend');
+
+  /* onpress시 라우터 이동 설정 */
+  // 통계 페이지로 이동
+  const onPressStatistics = () => {
+    router.push('/(main)/mypage/statistic');
   };
+
   // onpress시 editprofile로 이동
   const onPressEditProfile = () => {
     router.push('/(main)/mypage/editprofile');
@@ -71,6 +81,7 @@ const MypageScreen = () => {
       <View style={styles.header}>
         <View style={styles.headerleft}>
           <View style={styles.profileImage} />
+          <View style={styles.profileImageTop} />
           <Text style={styles.profileInfo}>
             <Text style={{ color: Colors.purple }}>Miya</Text>님과 함께한 지
             {'\n'}
@@ -88,40 +99,18 @@ const MypageScreen = () => {
         </View>
       </View>
       {/* 헤더 컨텐츠 */}
-      <View style={styles.headerContent}>
-        <HeaderContentItem
-          iconType="Feather"
-          iconName="archive"
-          text="음악 일기"
-          number="10개"
-          onPress={onPressArchive}
-        />
-        <HeaderContentItem
-          iconType="Feather"
-          iconName="send"
-          text="보낸 편지"
-          number="15개"
-          onPress={onPressLetter}
-        />
-        <HeaderContentItem
-          iconType="Ionicons"
-          iconName="mail-open-outline"
-          text="받은 편지"
-          number="7개"
-          onPress={onPressLetter}
-        />
-        <HeaderContentItem
-          iconType="FontAwesome5"
-          iconName="users"
-          text="내 친구"
-          number="20명"
-          onPress={onPressMyfriend}
-        />
-      </View>
+      <Text style={styles.dateText}>{`${year}년 ${month}월`}</Text>
+      <TouchableOpacity
+        style={styles.headerContent}
+        onPress={onPressStatistics}
+      >
+        <Feather name="pie-chart" size={24} color={Colors.grey1} />
+        <Text style={styles.textb2sb}>통계보기</Text>
+      </TouchableOpacity>
       {/* 바디1 */}
       <View style={styles.body1}>
         <TouchableOpacity style={styles.bodyRoute}>
-          <Text style={styles.textb2}>음악 취향</Text>
+          <Text style={styles.textb1}>내 음악 취향</Text>
           <View style={styles.musicflaver}>
             <Text style={styles.musicflaverText}>팝, 힙합 외 2</Text>
             <MaterialIcons
@@ -132,17 +121,43 @@ const MypageScreen = () => {
           </View>
         </TouchableOpacity>
         <View style={styles.bodyRoute}>
-          <Text style={styles.textb2}>새로운 장르 추천받기</Text>
+          <Text style={styles.textb1}>다양한 장르 추천받기</Text>
           <CustomToggle
-            isToggled={isToggled} // 초기 상태 전달
-            onToggleChange={handleToggleChange} // 토글 상태 변경 이벤트 핸들러
+            isToggled={isGenreToggled}
+            onToggleChange={handleToggleChange}
           />
         </View>
         {/* 바디2 */}
         <View style={styles.divider} />
         <View style={styles.body2}>
-          <BodyNavigator content="서비스 소개" onPress={() => {}} />
+          <Text style={styles.textb1}>서비스 알림</Text>
+          <View style={styles.bodyRoute}>
+            <Text style={styles.textb1Gray1}>일기 알림</Text>
+            <CustomToggle
+              isToggled={isDiaryToggled}
+              onToggleChange={handleDiaryToggleChange}
+            />
+          </View>
+         
+          {/* 기타 알림 */}
+          <View style={styles.bodyRoute}>
+            <Text style={styles.textb1Gray1}>기타 알림</Text>
+            <CustomToggle
+              isToggled={isEtcToggled}
+              onToggleChange={handleEtcToggleChange}
+            />
+          </View>
+          <Text style={styles.textb1}>마케팅 알림</Text>
+        </View>
+        {/* 바디2-1 */}
+        <View style={styles.divider} />
+        <View style={styles.body2}>
           <BodyNavigator content="문의 사항" onPress={onPressInquiry} />
+        </View>
+        {/* 바디2-2 */}
+        <View style={styles.divider} />
+        <View style={styles.body2}>
+          <BodyNavigator content="서비스 소개" onPress={() => {}} />
           <BodyNavigator content="오픈 라이센스" onPress={() => {}} />
           <BodyNavigator content="개인정보처리방침" onPress={() => {}} />
           <BodyNavigator content="이용 약관" onPress={() => {}} />
@@ -151,21 +166,22 @@ const MypageScreen = () => {
         <View style={styles.divider} />
         <View style={styles.body3}>
           <TouchableOpacity onPress={openLogoutModal}>
-            <Text style={styles.textb2}>로그아웃</Text>
+            <Text style={styles.textb1}>로그아웃</Text>
             <CustomAlert
               isVisible={isLogoutModalVisible}
               onConfirm={handleConfirm} // 확인 버튼 눌렀을 때 실행할 함수
               onCancel={closeLogoutModal}
-              firstLine="로그아웃시 어쩌고,, 라이팅 고민"
+              firstLine="로그아웃하시겠어요?"
               cancleMent="아니요, 안할래요"
               confirmMent="네, 할래요"
             />
           </TouchableOpacity>
           <TouchableOpacity onPress={onPressWithdrawal}>
-            <Text style={styles.secession}>회원탈퇴</Text>
+            <Text style={styles.secession}>탈퇴하기</Text>
           </TouchableOpacity>
         </View>
       </View>
+      <View style={{ paddingBottom: 120 }} />
     </ScrollView>
   );
 };
@@ -181,7 +197,8 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingVertical: 30,
+    paddingTop: 30,
+    paddingBottom: 24,
   },
   headerleft: {
     backgroundColor: Colors.black,
@@ -194,12 +211,22 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     backgroundColor: Colors.white,
   },
+  profileImageTop: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: Colors.purple,
+    position: 'absolute',
+    top: -6,
+    left: 40,
+  },
   profileInfo: {
-    paddingLeft: 10,
+    paddingLeft: 16,
     color: Colors.white,
     // fontfamily 확인 필요
     fontSize: 18,
     fontFamily: 'pret-b',
+    // ...Fonts.t1,
   },
   headerRight: {
     flexDirection: 'row',
@@ -220,16 +247,18 @@ const styles = StyleSheet.create({
     ...Fonts.btn,
   },
   headerContent: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  headerContentItem: {
-    backgroundColor: Colors.bg_light,
-    height: 88,
-    width: 76,
-    borderRadius: 6,
-    padding: 10,
+    justifyContent: 'center',
     alignItems: 'center',
+    height: 90,
+    marginTop: 7,
+    gap: 4,
+    backgroundColor: Colors.grey3,
+    borderRadius: 10,
+  },
+  dateText: {
+    color: Colors.white_alpha[50],
+    textAlign: 'left',
+    ...Fonts.b2_sb,
   },
   contentText: {
     paddingTop: 8,
@@ -242,12 +271,26 @@ const styles = StyleSheet.create({
     ...Fonts.b2_sb,
   },
   body1: {
-    paddingTop: 50,
-    gap: 16,
+    paddingTop: 34,
+    gap: 12,
   },
   bodyRoute: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textb1: {
+    color: Colors.white,
+    ...Fonts.b1,
+  },
+  textb1Gray1: {
+    color: Colors.grey1,
+    ...Fonts.b1,
+    paddingLeft: 14,
+  },
+  textb2sb: {
+    color: Colors.white,
+    ...Fonts.b2_sb,
   },
   textb2: {
     color: Colors.white,
@@ -265,18 +308,42 @@ const styles = StyleSheet.create({
   divider: {
     height: 1,
     backgroundColor: Colors.contents_light,
-    marginVertical: 16,
     marginHorizontal: -16,
+    marginVertical: 8,
   },
   body2: {
-    gap: 16,
+    gap: 12,
   },
   body3: {
     gap: 10,
   },
   secession: {
-    color: Colors.purple,
+    color: Colors.purple_alpha[50],
     ...Fonts.btn,
     textDecorationLine: 'underline',
+  },
+
+  // DatePicker
+  pickerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 20,
+    paddingBottom: 50,
+    borderRadius: 10,
+    backgroundColor: Colors.grey3,
+  },
+  pickerHeader: {
+    paddingHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  pickerTitle: {
+    color: Colors.white,
+    ...Fonts.t1,
+  },
+  btnText: {
+    color: Colors.purple,
+    ...Fonts.b1_sb,
   },
 });
