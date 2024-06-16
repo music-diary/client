e1ba455d2be16c47afbd1a9464d39300bf185949
TaegeonKim <aksen5240@gmail.com>
2024-06-17 02:28:46 +0900

    💄 Update bottom tab bar ui excluding blur effect implementation

--- src/app/(main)/_layout.tsx
+++ src/app/(main)/_layout.tsx
@@ -1,17 +1,16 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs, usePathname } from 'expo-router';
-import { BlurView } from 'expo-blur';
-import { StyleSheet } from 'react-native';
-import {
-  Foundation,
-  Feather,
-  FontAwesome6,
-  Fontisto,
-} from '@expo/vector-icons';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { Foundation, Feather, FontAwesome6 } from '@expo/vector-icons';
 import { useClientOnlyValue } from '@/hooks/useClientOnlyValue';
 import Colors from '@/constants/Colors';
+import TempBottomBar from 'assets/images/BottombarIcon';
+
+const ratio = 435 / 375;
+const currentWidth = Dimensions.get('window').width * ratio;
+const currentheight = (137 / 435) * currentWidth;
 
 export default function TabLayout() {
+  console.log('currentWidth', currentWidth);
   const path = usePathname();
   // 탭바 숨길 페이지 hide에 추가하면 됩니다..!
   const hide =
@@ -24,16 +23,22 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors.purple,
-        tabBarActiveBackgroundColor: Colors.white,
         tabBarInactiveTintColor: Colors.white,
         headerShown: useClientOnlyValue(false, true),
+        tabBarShowLabel: false,
         tabBarStyle: hide
           ? { display: 'none' }
           : { display: 'flex', ...styles.tabBarStyle },
         headerTransparent: true,
 
         tabBarBackground: () => (
-          <BlurView tint="light" intensity={10} style={styles.absoluteFill} />
+          <View style={styles.layout}>
+            <TempBottomBar
+              color={Colors.grey3}
+              width={currentWidth}
+              height={currentheight}
+            />
+          </View>
         ),
       }}
     >
@@ -41,79 +46,47 @@ export default function TabLayout() {
       <Tabs.Screen
         name="home"
         options={{
-          title: '홈',
+          title: '',
           headerShown: false,
           tabBarIcon: ({ color }) => (
             <Foundation name="home" size={21} color={color} />
           ),
           tabBarItemStyle: styles.item,
-          tabBarLabelStyle: {
-            fontFamily: 'pret-sb',
-            fontSize: 10,
-          },
         }}
       />
       <Tabs.Screen
         name="diary"
         options={{
-          title: '일기쓰기',
+          title: '',
           headerShown: false,
           tabBarIcon: ({ color }) => (
             <FontAwesome6 name="pen-nib" size={17} color={color} />
           ),
           tabBarStyle: { display: 'none' },
-          tabBarItemStyle: styles.item,
-          tabBarLabelStyle: {
-            fontFamily: 'pret-sb',
-            fontSize: 10,
-          },
+          tabBarItemStyle: styles.middleitem,
         }}
       />
       <Tabs.Screen
         name="archive"
         options={{
-          title: '아카이브',
+          title: '',
           headerShown: false,
           tabBarIcon: ({ color }) => (
             <Feather name="archive" size={18} color={color} />
           ),
-          tabBarItemStyle: styles.item,
-          tabBarLabelStyle: {
-            fontFamily: 'pret-sb',
-            fontSize: 10,
-          },
+          tabBarItemStyle: styles.rightitem,
         }}
       />
       <Tabs.Screen
         name="letter"
         options={{
-          title: '우편함',
-          headerShown: false,
-          // tabBarStyle: { display: "none" },  // 탭 바 숨기기
-          tabBarIcon: ({ color }) => (
-            <Fontisto name="email" size={22} color={color} />
-          ),
-          tabBarItemStyle: styles.item,
-          tabBarLabelStyle: {
-            fontFamily: 'pret-sb',
-            fontSize: 10,
-          },
+          href: null,
         }}
       />
       <Tabs.Screen
         name="mypage"
         options={{
-          title: '마이페이지',
-          headerShown: false,
-
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="user-o" size={18} color={color} />
-          ),
-          tabBarItemStyle: styles.item,
-          tabBarLabelStyle: {
-            fontFamily: 'pret-sb',
-            fontSize: 10,
-          },
+          href: null,
         }}
       />
     </Tabs>
@@ -123,33 +96,52 @@ export default function TabLayout() {
 const styles = StyleSheet.create({
   tabBarStyle: {
     flex: 1,
+    flexDirection: 'row',
+    position: 'absolute',
+    height: 103,
+    marginBottom: -34,
+    borderTopWidth: 0,
+    paddingVertical: 0,
+    zIndex: 1,
     justifyContent: 'space-between',
-    position: 'absolute',
-    marginBottom: 34,
-    paddingBottom: 8,
-    paddingTop: 8,
-    paddingHorizontal: 9,
-    height: 60,
-    marginHorizontal: 16,
-    borderRadius: 43,
-    borderTopWidth: 1,
-    borderTopColor: 'rgba(255, 255, 255, 0.3)',
-    borderColor: 'rgba(255, 255, 255, 0.3)',
-    borderWidth: 1,
-    backgroundColor: 'rgba(29, 29, 29, 0.2)',
+    backgroundColor: 'transparent',
   },
-  absoluteFill: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    height: 58,
-    borderRadius: 43,
-    overflow: 'hidden',
+
+  layout: {
+    // 중앙정렬
+    alignItems: 'center',
+    bottom: 34,
+    // shadow 설정
+    shadowColor: Colors.black,
+    shadowOpacity: 0.9,
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
   },
+
   item: {
+    paddingRight: 50,
+    paddingBottom: 20,
+  },
+  middleitem: {
+    backgroundColor: Colors.purple,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: -17,
+    left: '50%',
+    marginLeft: -30,
+    height: 60,
+    width: 60,
     borderRadius: 46,
-    paddingBottom: 4,
+    marginTop: -13,
+    zIndex: 1,
+    overflow: 'visible',
+  },
+
+  rightitem: {
+    paddingLeft: 50,
+    paddingBottom: 20,
   },
 });
