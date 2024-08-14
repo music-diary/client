45e8aaf5931a8acbdf939b274a97591a37810c42
Seongsoo Lee <seongsoo.dev@gmail.com>
2024-08-14 11:26:49 +0900

    ✨ Improve archive card: update emotion display and handle template rendering (#61)

--- src/constants/data/index.ts
+++ src/constants/data/index.ts
@@ -1,7 +1,6 @@
 export { genres } from './genres'; // 추후 삭제해야함
 export { genders } from './genders';
 export { terms } from './terms';
-export { templates } from './templates';
 export { splashOptions } from './splash';
 export { moodIcons } from './mood-icons';
 export { emotionColor } from './emotion-colors';
