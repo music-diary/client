ce2137c710b0af5fa501a1bbd9e612eba9f39ec9
TaegeonKim <aksen5240@gmail.com>
2024-08-29 19:05:15 +0900

    :truck: Moved the tutorial page to the (tutorial) group folder

---
+++ src/app/(tutorial)/diary.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const diary = () => {
+  return (
+    <View>
+      <Text>diary</Text>
+    </View>
+  );
+};
+
+export default diary;
