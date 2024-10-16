26ff94ec4dbb3817500d645845eccba8ad269fe1
seongsoo96 <seongsoo.dev@gmail.com>
2024-10-16 23:19:27 +0900

    🔧 Fix encryption config and remove alert

--- src/hooks/usePushNotifications.ts
+++ src/hooks/usePushNotifications.ts
@@ -42,7 +42,7 @@ export const usePushNotifications = (): PushNotificationState => {
         finalStatus = status;
       }
       if (finalStatus !== 'granted') {
-        alert('Failed to get push token for push notification');
+        console.warn('Failed to get push token for push notification');
         return;
       }
 
