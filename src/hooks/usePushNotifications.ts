f6af7218421d43e4941a959bc82a4573b0fe7d8e
seongsoo96 <seongsoo.dev@gmail.com>
2024-10-17 09:17:58 +0900

    🔧 Remove comment

--- src/hooks/usePushNotifications.ts
+++ src/hooks/usePushNotifications.ts
@@ -42,7 +42,6 @@ export const usePushNotifications = (): PushNotificationState => {
         finalStatus = status;
       }
       if (finalStatus !== 'granted') {
-        console.warn('Failed to get push token for push notification');
         return;
       }
 
